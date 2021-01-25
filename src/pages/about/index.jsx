5da70d6d6a4220690a978d60f8f44b34b9f58654
Adam Jackson <akjackson1@gmail.com>
2021-01-25 11:39:36 -0500

    Update index.jsx

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -7,7 +7,7 @@ function About() {
 		<div className='container page-wrapper'>
 			<div className="page content">
 				<img src='/me.jpg' alt='' className='bio-image'/>
-				<p>I'm a web developer and biostatistics graduate student, but what really gives me a thrill is chasing rare birds around world. I developed an interest in bird photography at the age of 12 while living in Tasmania and started getting more serious about birding in 2016.</p>
+				<p>I'm a web developer that loves chasing rare birds around world. I developed an interest in bird photography at the age of 12 while living in Tasmania and started getting more serious about birding in 2016.</p>
 				<p>Want to get in touch? Contact me via email: <a href='mailto:adam@rawcomposition.com'>adam@rawcomposition.com</a></p>
 			</div>
 			<Sidebar/>
