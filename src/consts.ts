205ae6b5d1d3b405ccbc59d66c098acf9674ea34
Adam Jackson <akjackson1@gmail.com>
2026-07-05 11:38:03 -0700

    Revise progress page

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,8 @@ export const SITE_DESCRIPTION = 'Bird Photography by Adam Jackson';
 
 // Endemics data (island-endemics.ts) is still a work in progress
 export const SHOW_ENDEMICS = false;
+
+// Macaulay asset ID of a recent lifer to feature in the progress hero and home
+// banner instead of the latest addition (in case the latest photo is a dud).
+// Set to null to use the latest lifer.
+export const HERO_IMG_OVERRIDE: number | null = null;
